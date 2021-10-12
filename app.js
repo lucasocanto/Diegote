894669e0b38a972cfcc6a536b404c7ee61e46213
unknown <lucasocanto1996@gmail.com>
2021-10-12 18:52:14 -0300

    agregando header, falta dejarlo del todo lindo y funcionalidades

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(publicPath))
 app.use(express.urlencoded())
 
 app.get("/", (req,res)=>{
-    let file = path.join(__dirname,"./views/login.html") 
+    let file = path.join(__dirname,"./views/home.html") 
     res.sendFile(file)
 })
 
